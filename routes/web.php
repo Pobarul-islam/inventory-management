8839cc0592d14fa87203076c6443b6f13e445de4
Pobarul-islam <pobarul3@gmail.com>
2024-03-08 17:23:13 +0600

    portfolio layout setup and admin controller and page created

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminHomeController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Admin 
+
+Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home');
