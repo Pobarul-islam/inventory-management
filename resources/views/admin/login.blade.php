2a86c622a58d74065c6b7d606731153334a7299a
Pobarul-islam <pobarul3@gmail.com>
2024-03-13 19:37:39 +0600

    forget password email send and email click linke problem solved

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -27,6 +27,10 @@
                             class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                             <div class="card card-primary border-box">
                                 <div class="card-header card-header-auth">
+                                     @if(session()->get('success'))
+                                             <div class="text-success">{{ session()->get('success')}}</div>
+                                            @endif
+
                                     <h4 class="text-center">Admin Panel Login</h4>
                                 </div>
                                 <div class="card-body card-body-auth">
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
 
 // Admin 
 
-Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home');
+Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home')->middleware(('admin:admin'));
 Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin_login');
 
 Route::post('/admin/login-submit', [AdminLoginController::class, 'login_submit'])->name('admin_login_submit');
@@ -20,3 +20,4 @@ Route::post('/admin/login-submit', [AdminLoginController::class, 'login_submit']
 Route::get('/admin/logout', [AdminLoginController::class, 'logout'])->name('admin_logout');
 
 Route::get('/admin/forget-password', [AdminLoginController::class, 'forget_password'])->name('admin_forget_password');
+Route::post('/admin/forget-password-submit', [AdminLoginController::class, 'forget_password_submit'])->name('admin_forget_password_submit');
