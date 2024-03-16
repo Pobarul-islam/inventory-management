4996a8efbf86b7da78f5de9ac69b1b1bbcb46009
Pobarul-islam <pobarul3@gmail.com>
2024-03-16 16:51:37 +0600

    convert admin panel bootstrap 5

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,41 +1,48 @@
  <div class="main-sidebar">
-                <aside id="sidebar-wrapper">
-                    <div class="sidebar-brand">
-                        <a href="{{ route('admin_home') }}">Admin Panel</a>
-                    </div>
-                    <div class="sidebar-brand sidebar-brand-sm">
-                        <a href="{{ route('admin_home') }}"></a>
-                    </div>
+     <aside id="sidebar-wrapper">
+         <div class="sidebar-brand">
+             <a href="{{ route('admin_home') }}">Admin Panel</a>
+         </div>
+         <div class="sidebar-brand sidebar-brand-sm">
+             <a href="{{ route('admin_home') }}"></a>
+         </div>
 
-                    <ul class="sidebar-menu">
+         <ul class="sidebar-menu">
 
-                        <li class="active"><a class="nav-link" href="{{ route('admin_home') }}"><i class="fas fa-hand-point-right"></i>
-                                <span>Dashboard</span></a></li>
+             <li class="active"><a class="nav-link" href="{{ route('admin_home') }}" data-bs-toggle="tooltip"
+                     data-bs-placement="right"   data-bs-title="Dashboard">
+
+                     <i class="fas fa-hand-point-right"></i>
+                     <span>Dashboard</span></a></li>
 
 
-                        <li class="nav-item dropdown active">
-                            <a href="#" class="nav-link has-dropdown"><i
-                                    class="fas fa-hand-point-right"></i><span>Dropdown Items</span></a>
-                            <ul class="dropdown-menu">
-                                <li class="active"><a class="nav-link" href=""><i class="fas fa-angle-right"></i>
-                                        Item 1</a></li>
-                                <li class=""><a class="nav-link" href=""><i class="fas fa-angle-right"></i>
-                                        Item 2</a></li>
-                            </ul>
-                        </li>
+             <li class="nav-item dropdown active">
+                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-hand-point-right"></i><span>Dropdown
+                         Items</span></a>
+                 <ul class="dropdown-menu">
+                     <li class="active"><a class="nav-link" href=""><i class="fas fa-angle-right"></i>
+                             Item 1</a></li>
+                     <li class=""><a class="nav-link" href=""><i class="fas fa-angle-right"></i>
+                             Item 2</a></li>
+                 </ul>
+             </li>
 
-                        <li class=""><a class="nav-link" href="setting.html"><i
-                                    class="fas fa-hand-point-right"></i> <span>Setting</span></a></li>
+             <li class=""><a class="nav-link" href="setting.html "><i class="fas fa-hand-point-right" data-bs-toggle="tooltip"
+                     data-bs-placement="right"   data-bs-title="Settings"></i>
+                     <span>Settings</span></a></li>
 
-                        <li class=""><a class="nav-link" href="form.html"><i class="fas fa-hand-point-right"></i>
-                                <span>Form</span></a></li>
+             <li class=""><a class="nav-link" href="form.html"><i class="fas fa-hand-point-right" data-bs-toggle="tooltip"
+                     data-bs-placement="right"   data-bs-title="Form"></i>
+                     <span>Form</span></a></li>
 
-                        <li class=""><a class="nav-link" href="table.html"><i class="fas fa-hand-point-right"></i>
-                                <span>Table</span></a></li>
+             <li class=""><a class="nav-link" href="table.html"><i class="fas fa-hand-point-right" data-bs-toggle="tooltip"
+                     data-bs-placement="right"   data-bs-title="Table"></i>
+                     <span>Table</span></a></li>
 
-                        <li class=""><a class="nav-link" href="invoice.html"><i
-                                    class="fas fa-hand-point-right"></i> <span>Invoice</span></a></li>
+             <li class=""><a class="nav-link" href="invoice.html"><i class="fas fa-hand-point-right" data-bs-toggle="tooltip"
+                     data-bs-placement="right"   data-bs-title="Invoice"></i>
+                     <span>Invoice</span></a></li>
 
-                    </ul>
-                </aside>
-            </div>
+         </ul>
+     </aside>
+ </div>
