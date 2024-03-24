9cf4804d5b8e0fce09c8e390950dcdd8e57491cf
Pobarul-islam <pobarul3@gmail.com>
2024-03-24 09:30:42 +0600

    home page table create

---
+++ database/migrations/2024_03_24_032056_create_home_page_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('home_page_items', function (Blueprint $table) {
+            $table->id();
+            $table->text('banner_title')->nullable();
+            $table->text('banner_person_name');
+            $table->text('banner_person_designation')->nullable();
+            $table->text('banner_person_description')->nullable();
+            $table->text('banner_person_text')->nullable();
+            $table->text('banner_person_button_url')->nullable();
+            $table->text('banner_person_photo');
+          
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('home_page_items');
+    }
+};
